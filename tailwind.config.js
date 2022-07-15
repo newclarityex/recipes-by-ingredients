c7a425ac4e7c97c67456af2d7b106bb7de585bb4
newclarityex <69740123+newclarityex@users.noreply.github.com>
2022-07-14 22:24:52 -0400

    Basic Layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            darkText: "#000000aa",
+            background: "#fff",
+            primary: "#00000080",
+            secondary: "#00000080",
+        }
+    },
   },
   plugins: [],
 };
